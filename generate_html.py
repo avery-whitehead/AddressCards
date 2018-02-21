2a4fe3c9f796c55e9f1a9fb50e789761ee2a91a1
James Whitehead <james.whitehead@hambleton.gov.uk>
2018-02-21 11:05:43 +0000

    Added SQL functionality

--- generate_html.py
+++ generate_html.py
@@ -1,3 +1,5 @@
+import pyodbc
+
 class Property:
     def __init__(self, name, firstAddr, secondAddr, town, county, postcode):
         self.name = name
@@ -24,6 +26,23 @@ class Property:
             out_file.write(''.join(html))
 
 if __name__ == '__main__':
+    # Connect to SQL Server
+    conn = pyodbc.connect(
+        r'DRIVER={ODBC Driver 13 for SQL Server};'
+        r'SERVER=CCVSQL12\SQLInstance2;'
+        r'DATABASE=Images;'
+        r'UID=PACommentAutomation;'
+        r'PWD=T5b}rh~Dq<kY2'
+    )
+
+    # Execute and read SQL query
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM dbo.FOLDER3 WHERE FOLDER3_REF = '010001280513'")
+    rows = cursor.fetchall()
+    for item in rows[0]:
+        print(item)
+
+    # Create Property objects
     prop = Property('James Whitehead', '9 White Rose House', 'Ainderby Gardens', 'Northallerton', 'North Yorkshire', 'DL7 8GT')
     html = prop.build_html()
     prop.export_html(html)
