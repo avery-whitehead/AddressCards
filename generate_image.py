e061ae2a7cfc94f0be4eecd4ce527622e3677e5b
james-whitehead <jameswhthead@gmail.com>
2018-03-15 11:52:26 +0000

    Working placement of text across 4x4 A3, still need to tweak coordinate and dimension values

--- generate_image.py
+++ generate_image.py
@@ -65,6 +65,35 @@ class Calendar:
             self.gw_string = '{} {}, {} May'.format(gw_day, str(dates[gw_day]), str(dates[gw_day] + 14))
         self.gls_string = '{} {}, {} May'.format(gls_day, str(dates[gls_day]), str(dates[gls_day] + 14))
 
+class TextBox:
+    """ Represents an image to hold some collection data in.
+
+    Args:
+        text_image (:obj:PIL.Image.Image): An image containing the text
+        x_offset(int): How far along the x-coordinate the base image is
+        y_offset(int): How far along the y-coordinate the base image is
+    """
+    def __init__(self, text_image, x_coord, y_coord, position):
+        self.text_image = text_image
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        # Top left
+        if position == 0:
+            self.x_offset = 0
+            self.y_offset = 0
+        # Top right
+        if position == 1:
+            self.x_offset = 2480
+            self.y_offset = 0
+        # Bottom left
+        if position == 2:
+            self.x_offset = 0
+            self.y_offset = 1754
+        # Bottom right
+        if position == 3:
+            self.x_offset = 2480
+            self.y_offset = 1754
+
 
 def build_postcard(conn, uprn):
     """ Creates a Postcard object for a property
@@ -81,7 +110,8 @@ def build_postcard(conn, uprn):
         cursor.execute(query, uprn)
         row = cursor.fetchone()
         # Builds a Calendar object based on the collection information
-        cal_obj = Calendar(row.REFDay, row.REFWeek, row.RECYDay, row.RECYWeek, row.GWDay, row.GWWeek, row.RECYDay, row.RECYWeek)
+        cal_obj = Calendar(
+            row.REFDay, row.REFWeek, row.RECYDay, row.RECYWeek, row.GWDay, row.GWWeek, row.RECYDay, row.RECYWeek)
         # Builds a Postcard object based on the address and collection calendar
         post_obj = Postcard(row.addressBlock, cal_obj)
 
@@ -91,42 +121,60 @@ def build_postcard(conn, uprn):
         return post_obj
 
 
-def build_image(post_obj_list, uprn_list):
-    class Positions(Enum):
-        TOP_LEFT = 0
-        TOP_RIGHT = 1
-        BOTTOM_LEFT = 2
-        BOTTOM_RIGHT = 3
-
-    addr_font = ImageFont.truetype('arial.ttf', 60)
-    cal_font = ImageFont.truetype('futura bold condensed italic bt.ttf', 65)
-
+def build_all_images(postcard_list, uprn_list):
     addr_img = Image.open('./out/postcard-front-4x4.png').convert('RGBA')
     cal_img = Image.open('./out/postcard-back-4x4.png').convert('RGBA')
+    addr_img.load()
+    cal_img.load()
+    for position, postcard in enumerate(postcard_list):
+        text_boxes = build_one_image(postcard, position)
+        #print('Image: {}\nX: {}\nY: {}\nX (with offset):{}\nY (with offset){}'.format(
+            #text_box.text_image, text_box.x_coord, text_box.y_coord, text_box.x_coord + text_box.x_offset, text_box.y_coord + text_box.y_offset))
+        # Overlays the rotated box on to the image
+        for text_box in text_boxes:
+            cal_img.paste(ImageOps.colorize(
+                text_box.text_image, (0, 0, 0), (255, 255, 255)),
+                ((text_box.x_coord + text_box.x_offset), (text_box.y_coord + text_box.y_offset)), text_box.text_image)
+    out_file = '{}.png'.format('-'.join(uprn_list))
+    cal_img.save(out_file)
 
-    for position, postcard in enumerate(post_obj_list):
-        if position == Positions.TOP_LEFT.value:
-            # Wraps the string over separate lines
-            string = textwrap.wrap(postcard.calendar.ref_string, width=9)
-            # Creates a box to hold the text in
-            text_box = Image.new('L', (250, 330))
-            text_draw = ImageDraw.Draw(text_box)
-            # The current height in the box
-            curr_h = 50
-            # The padding between lines
-            pad = 10
-            for line in string:
-                # Calculates the width and height of each line
-                w, h = text_draw.textsize(line, font=cal_font)
-                # Writes the text to the box using the width and height so it's centre aligned
-                text_draw.text(((250 - w) / 2, curr_h), line, font=cal_font, fill=255)
-                # Move the height cursor down
-                curr_h += h + pad
-            # Rotates the box
-            rotate = text_box.rotate(4, resample=Image.BICUBIC, expand=True)
-            # Overlays the rotated box on to the image
-            cal_img.paste(ImageOps.colorize(rotate, (0, 0, 0), (255, 255, 255)), (290, 890), rotate)
-    cal_img.save('new.png')
+
+def build_one_image(postcard, position):
+    # Wraps the strings over separate lines
+    ref_string = textwrap.wrap(postcard.calendar.ref_string, width=9)
+    recy_string = textwrap.wrap(postcard.calendar.recy_string, width=9)
+    gw_string = textwrap.wrap(postcard.calendar.gw_string, width=9)
+    gls_string = textwrap.wrap(postcard.calendar.gls_string, width=12)
+
+    # Creates the text boxes
+    ref_text = build_text(ref_string, 260, 350, 250, 890, position, 4)
+    recy_text = build_text(recy_string, 260, 350, 860, 890, position, 4)
+    gw_text = build_text(gw_string, 260, 350, 2000, 890, position, 4)
+    gls_text = build_text(gls_string, 310, 180, 1420, 980, position, 2)
+
+    return [ref_text, recy_text, gw_text, gls_text]
+
+
+def build_text(string, width, height, x_coord, y_coord, position, rotation):
+    addr_font = ImageFont.truetype('arial.ttf', 60)
+    cal_font = ImageFont.truetype('futura bold condensed italic bt.ttf', 57)
+    # Creates a box to hold the text in
+    text_box = Image.new('L', (width, height))
+    text_draw = ImageDraw.Draw(text_box)
+    # The current height in the box
+    curr_h = 50
+    # The padding between lines
+    pad = 10
+    for line in string:
+        # Calculates the width and height of each line
+        w, h = text_draw.textsize(line, font=cal_font)
+        # Writes the text to the box using the width and height so it's centre aligned
+        text_draw.text(((250 - w) / 2, curr_h), line, font=cal_font, fill=255)
+        # Move the height cursor down
+        curr_h += h + pad
+    # Rotates the box
+    return TextBox(text_box.rotate(rotation, resample=Image.BICUBIC, expand=True), x_coord, y_coord, position)
+
 
 if __name__ == '__main__':
     pyodbc.pooling = False
@@ -145,6 +193,6 @@ if __name__ == '__main__':
         for uprn in uprn_list:
             postcard = build_postcard(conn, uprn)
             postcard_list.append(postcard)
-        build_image(postcard_list, uprn_list)
+        build_all_images(postcard_list, uprn_list)
 
     conn.close()
