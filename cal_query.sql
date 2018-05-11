13fc4d5c842af81a99db971acfe6d050451b6f0f
james-whitehead <jameswhthead@gmail.com>
2018-05-11 14:03:06 +0100

    Cleaned up style of SQL queries

--- cal_query.sql
+++ cal_query.sql
@@ -1,52 +1,59 @@
-select
-       p.uprn
-       ,replace(replace(l.ADDRESS_BLOCK_ORG,'North Yorkshire'+char(13)+char(10),''),char(13)+char(10),'<br>') as addressBlock
-       ,case
-              when ref.ScheduleDayID < 8 then 0
-              when ref.ScheduleDayID > 7 then 1
-       end as REFWeek
-       ,case
-              when ref.ScheduleDayID in (1,8) then 'Monday'
-              when ref.ScheduleDayID in (2,9) then 'Tuesday'
-              when ref.ScheduleDayID in (3,10) then 'Wednesday'
-              when ref.ScheduleDayID in (4,11) then 'Thursday'
-              when ref.ScheduleDayID in (5,12) then 'Friday'
-       end as REFDay
-       ,case
-              when recy.ScheduleDayID < 8 then 0
-              when recy.ScheduleDayID > 7 then 1
-       end as RECYWeek
-       ,case
-              when recy.ScheduleDayID in (1,8) then 'Monday'
-              when recy.ScheduleDayID in (2,9) then 'Tuesday'
-              when recy.ScheduleDayID in (3,10) then 'Wednesday'
-              when recy.ScheduleDayID in (4,11) then 'Thursday'
-              when recy.ScheduleDayID in (5,12) then 'Friday'
-       end as RECYDay
-       ,case
-              when gw.ScheduleDayID < 8 then 0
-              when gw.ScheduleDayID > 7 then 1
-       end as GWWeek
-       ,case
-              when gw.ScheduleDayID in (1,8) then 'Monday'
-              when gw.ScheduleDayID in (2,9) then 'Tuesday'
-              when gw.ScheduleDayID in (3,10) then 'Wednesday'
-              when gw.ScheduleDayID in (4,11) then 'Thursday'
-              when gw.ScheduleDayID in (5,12) then 'Friday'
-       end as GWDay
-from properties p
-       join LLPG_ADDRESS_CURRENT_SPATIAL l
-                     on p.uprn=l.uprn
-       left join (select * from PropertyServiceRounds where serviceid = 'REF' and RoundEra=2) psr_ref
-              on p.uprn=psr_ref.uprn
-       left join (select * from rounds where serviceid = 'REF' and RoundEra=2)  ref
-              on psr_ref.RoundID=ref.RoundID
-       left join (select * from PropertyServiceRounds where serviceid = 'RECY' and RoundEra=2) psr_recy
-              on p.uprn=psr_recy.uprn
-       left join (select * from rounds where serviceid = 'RECY' and RoundEra=2)  recy
-              on psr_recy.RoundID=recy.RoundID
-       left join (select * from PropertyServiceRounds where serviceid = 'GW' and RoundEra=2) psr_gw
-              on p.uprn=psr_gw.uprn
-       left join (select * from rounds where serviceid = 'GW' and RoundEra=2)  gw
-              on psr_gw.RoundID=gw.RoundID
-where psr_ref.RoundEra=2 and p.uprn in (?)
+SELECT
+    p.uprn,
+    REPLACE(REPLACE(l.ADDRESS_BLOCK_ORG, 'North Yorkshire' + CHAR(13) + CHAR(10), ''), CHAR(13) + CHAR(10), '<br>') AS addressBlock,
+        CASE
+            WHEN ref.ScheduleDayID < 8 then 0
+            WHEN ref.ScheduleDayID > 7 then 1
+        END
+    AS REFWeek,
+        CASE
+            WHEN ref.ScheduleDayID IN (1, 8) THEN 'Monday'
+            WHEN ref.ScheduleDayID IN (2, 9) THEN 'Tuesday'
+            WHEN ref.ScheduleDayID IN (3, 10) THEN 'Wednesday'
+            WHEN ref.ScheduleDayID IN (4, 11) THEN 'Thursday'
+            WHEN ref.ScheduleDayID IN (5, 12) THEN 'Friday'
+        END
+    AS REFDay,
+        CASE
+            WHEN recy.ScheduleDayID < 8 THEN 0
+            WHEN recy.ScheduleDayID > 7 THEN 1
+       END
+    AS RECYWeek,
+        CASE
+            WHEN recy.ScheduleDayID IN (1, 8) THEN 'Monday'
+            WHEN recy.ScheduleDayID IN (2, 9) THEN 'Tuesday'
+            WHEN recy.ScheduleDayID IN (3, 10) THEN 'Wednesday'
+            WHEN recy.ScheduleDayID IN (4, 11) THEN 'Thursday'
+            WHEN recy.ScheduleDayID IN (5, 12) THEN 'Friday'
+       END
+    AS RECYDay,
+        CASE
+            WHEN gw.ScheduleDayID < 8 THEN 0
+            WHEN gw.ScheduleDayID > 7 THEN 1
+        END
+    AS GWWeek,
+        CASE
+            WHEN gw.ScheduleDayID IN (1, 8) THEN 'Monday'
+            WHEN gw.ScheduleDayID IN (2, 9) THEN 'Tuesday'
+            WHEN gw.ScheduleDayID IN (3, 10) THEN 'Wednesday'
+            WHEN gw.ScheduleDayID IN (4, 11) THEN 'Thursday'
+            WHEN gw.ScheduleDayID IN (5, 12) THEN 'Friday'
+       END
+    AS GWDay
+FROM properties p
+    JOIN LLPG_ADDRESS_CURRENT_SPATIAL l
+        ON p.uprn = l.uprn
+    LEFT JOIN (SELECT * FROM PropertyServiceRounds WHERE serviceid = 'REF' AND RoundEra = 2) psr_ref
+        ON p.uprn = psr_ref.uprn
+    LEFT JOIN (SELECT * FROM rounds WHERE serviceid = 'REF' AND RoundEra = 2) ref
+        ON psr_ref.RoundID = ref.RoundID
+    LEFT JOIN (SELECT * FROM PropertyServiceRounds WHERE serviceid = 'RECY' AND RoundEra = 2) psr_recy
+        ON p.uprn = psr_recy.uprn
+    LEFT JOIN (SELECT * FROM rounds WHERE serviceid = 'RECY' AND RoundEra = 2) recy
+        ON psr_recy.RoundID = recy.RoundID
+    LEFT JOIN (SELECT * FROM PropertyServiceRounds WHERE serviceid = 'GW' AND RoundEra = 2) psr_gw
+        ON p.uprn = psr_gw.uprn
+    LEFT JOIN (SELECT * FROM rounds WHERE serviceid = 'GW' AND RoundEra = 2) gw
+        ON psr_gw.RoundID = gw.RoundID
+WHERE psr_ref.RoundEra = 2
+AND p.uprn IN (?)
--- uprn_query.sql
+++ uprn_query.sql
@@ -1,27 +1,27 @@
-select TOP(1000)
-       p.uprn
-from properties p
-       join LLPG_ADDRESS_CURRENT_SPATIAL l
-                     on p.uprn=l.uprn
-       left join (select * from PropertyServiceRounds where serviceid = 'REF' and RoundEra=2) psr_ref
-              on p.uprn=psr_ref.uprn
-       left join (select * from rounds where serviceid = 'REF' and RoundEra=2)  ref
-              on psr_ref.RoundID=ref.RoundID
-       left join (select * from PropertyServiceRounds where serviceid = 'RECY' and RoundEra=2) psr_recy
-              on p.uprn=psr_recy.uprn
-       left join (select * from rounds where serviceid = 'RECY' and RoundEra=2)  recy
-              on psr_recy.RoundID=recy.RoundID
-       left join (select * from PropertyServiceRounds where serviceid = 'GW' and RoundEra=2) psr_gw
-              on p.uprn=psr_gw.uprn
-       left join (select * from rounds where serviceid = 'GW' and RoundEra=2)  gw
-              on psr_gw.RoundID=gw.RoundID
-       left join (select * from PropertyServiceRounds where serviceid = 'MIX' and RoundEra=2) psr_mix
-              on p.uprn=psr_mix.uprn
-       left join (select * from rounds where serviceid = 'MIX' and RoundEra=2)  mix
-              on psr_mix.RoundID=mix.RoundID
-       left join (select * from PropertyServiceRounds where serviceid = 'GLASS' and RoundEra=2) psr_glass
-              on p.uprn=psr_glass.uprn
-       left join (select * from rounds where serviceid = 'GLASS' and RoundEra=2)  glass
-              on psr_glass.RoundID=glass.RoundID
-where psr_ref.RoundEra=2
-and mix.ScheduleDayID is null and glass.ScheduleDayID is null
+SELECT p.uprn
+FROM properties p
+    JOIN LLPG_ADDRESS_CURRENT_SPATIAL l
+        ON p.uprn = l.uprn
+    LEFT JOIN (SELECT * FROM PropertyServiceRounds WHERE serviceid = 'REF' AND RoundEra = 2) psr_ref
+        ON p.uprn = psr_ref.uprn
+    LEFT JOIN (SELECT * FROM rounds where serviceid = 'REF' AND RoundEra=2) ref
+        ON psr_ref.RoundID = ref.RoundID
+    LEFT JOIN (SELECT * FROM PropertyServiceRounds WHERE serviceid = 'RECY' AND RoundEra = 2) psr_recy
+        ON p.uprn = psr_recy.uprn
+    LEFT JOIN (SELECT * FROM rounds WHERE serviceid = 'RECY' and RoundEra = 2) recy
+        ON psr_recy.RoundID = recy.RoundID
+    LEFT JOIN (SELECT * FROM PropertyServiceRounds WHERE serviceid = 'GW' AND RoundEra = 2) psr_gw
+        ON p.uprn = psr_gw.uprn
+    LEFT JOIN (SELECT * FROM rounds WHERE serviceid = 'GW' AND RoundEra = 2) gw
+        ON psr_gw.RoundID = gw.RoundID
+    LEFT JOIN (SELECT * FROM PropertyServiceRounds WHERE serviceid = 'MIX' AND RoundEra = 2) psr_mix
+        ON p.uprn = psr_mix.uprn
+    LEFT JOIN (SELECT * FROM rounds WHERE serviceid = 'MIX' and RoundEra = 2) mix
+        ON psr_mix.RoundID = mix.RoundID
+    LEFT JOIN (SELECT * FROM PropertyServiceRounds WHERE serviceid = 'GLASS' AND RoundEra = 2) psr_glass
+        ON p.uprn = psr_glass.uprn
+    LEFT JOIN (SELECT * FROM rounds WHERE serviceid = 'GLASS' and RoundEra = 2) glass
+        ON psr_glass.RoundID = glass.RoundID
+WHERE psr_ref.RoundEra = 2
+AND mix.ScheduleDayID IS NULL
+AND glass.ScheduleDayID IS NULL
